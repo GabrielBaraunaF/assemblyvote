e2aa966b37a56cfbba15ef58e04fbbe4c3c1cf62
gabrielbarauna <gbferreira08@gmail.com>
2024-09-02 14:33:18 -0300

    initial commit

---
+++ src/test/java/br/com/solutis/assemblyvote/AssemblyVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solutis.assemblyvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblyVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
